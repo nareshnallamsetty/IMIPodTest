1b9aaeac5e5939abece0a34aa58b6fc1f6a9bd2f
Naresh Nallamsetty <naresh.n@imimobile.com>
2019-05-28 17:15:06 +0530

    Initial Commit

---
+++ TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Naresh Nallamsetty on 28/05/19.
+//  Copyright © 2019 IMIMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
